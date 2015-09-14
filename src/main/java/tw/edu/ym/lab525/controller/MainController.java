355657837f3da639b1fd4d6acb63cf7cb65f0e15
Ming-Jheng Li <mjli@ym.edu.tw>
2015-09-14 20:05:38 +0800

    step4-withCreate

--- src/main/java/tw/edu/ym/lab525/controller/MainController.java
+++ src/main/java/tw/edu/ym/lab525/controller/MainController.java
@@ -28,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import tw.edu.ym.lab525.entity.Book;
 import tw.edu.ym.lab525.repository.BookRepository;
 
 @RequestMapping("/books")
@@ -47,9 +48,35 @@ public class MainController {
   @RequestMapping(value = "/lookup", method = RequestMethod.GET)
   public String lookup(ModelMap map,
       @RequestParam(value = "isbn") String isbn) {
-    map.addAttribute("books", bookRepo.findByIsbn(isbn));
+    map.addAttribute("books",
+        isbn.equals("") ? bookRepo.findAll() : bookRepo.findByIsbn(isbn));
 
     return "books";
+  }
+
+  @RequestMapping(value = "", method = RequestMethod.POST)
+  public String create(ModelMap map, @RequestParam(value = "name") String name,
+      @RequestParam(value = "isbn") String isbn,
+      @RequestParam(value = "author") String author,
+      @RequestParam(value = "price") Integer price) {
+
+    if (name.equals("") || isbn.equals("") || author.equals("")
+        || price == null) {
+      map.addAttribute("errorMessage", "請勿輸入空值");
+      return "books";
+    } else if (bookRepo.findByIsbn(isbn) != null) {
+      map.addAttribute("errorMessage", "ISBN 重複");
+      return "books";
+    } else {
+      Book book = new Book();
+      book.setName(name);
+      book.setIsbn(isbn);
+      book.setAuthor(author);
+      book.setPrice(price);
+      bookRepo.save(book);
+
+      return "redirect:/books";
+    }
   }
 
   @RequestMapping("/hello")
