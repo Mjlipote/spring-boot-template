490d0fd7e2da6a28db2f71aeca8dba20ba8117a1
Ming-Jheng Li <mjli0721@gmail.com>
2016-03-31 15:05:39 +0800

    Add GoogleChart DataTable

---
+++ src/main/java/tw/edu/ym/lab525/chart/MainTest.java
@@ -0,0 +1,52 @@
+package tw.edu.ym.lab525.chart;
+/// *
+// *
+// * @author Ming-Jheng Li
+// *
+// *
+// * Copyright 2015 Ming-Jheng Li
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// * use this file except in compliance with the License. You may obtain a copy
+/// of
+// * the License at
+// *
+// * http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// * License for the specific language governing permissions and limitations
+/// under
+// * the License.
+// *
+// */
+// package tw.guid.local.google.chart;
+//
+// import java.io.IOException;
+// import java.net.URISyntaxException;
+//
+// public class MainTest {
+//
+// /**
+// * @param args
+// * @throws URISyntaxException
+// * @throws IOException
+// */
+// public static void main(String[] args)
+// throws URISyntaxException, IOException {
+//
+// DataTable dataTable = new DataTable();
+//
+// dataTable.addColumn(new ColumnDescription("task", "Task", Type.STRING),
+// new ColumnDescription("hours", "Hours per Day", Type.NUMBER));
+//
+// dataTable.addRow("Work", 11);
+// dataTable.addRow("Eat", 2);
+// dataTable.addRow("Commute", 2);
+//
+// System.out.println(dataTable.toJson());
+//
+// }
+//
+// }
