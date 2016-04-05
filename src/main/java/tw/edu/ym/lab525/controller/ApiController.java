ded7f11c87d917a23431b401aea56663d6ad64a8
Ming-Jheng Li <mjli0721@gmail.com>
2016-04-05 16:36:15 +0800

    Add @CrossOrigin to solve that JSON can't cross

--- src/main/java/tw/edu/ym/lab525/controller/ApiController.java
+++ src/main/java/tw/edu/ym/lab525/controller/ApiController.java
@@ -20,12 +20,14 @@ package tw.edu.ym.lab525.controller;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import tw.edu.ym.lab525.chart.DataTable;
 import tw.edu.ym.lab525.service.ApiService;
 
+@CrossOrigin
 @RequestMapping("/api")
 @RestController
 public class ApiController {
