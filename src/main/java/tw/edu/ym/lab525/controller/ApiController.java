7e07b6429dacb5f36025001c4b9e14d6e95af2e4
Ming-Jheng Li <mjli0721@gmail.com>
2017-06-08 17:31:41 +0800

    Add downloadExcel to ApiController

--- src/main/java/tw/edu/ym/lab525/controller/ApiController.java
+++ src/main/java/tw/edu/ym/lab525/controller/ApiController.java
@@ -17,13 +17,31 @@
  */
 package tw.edu.ym.lab525.controller;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.util.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.wnameless.workbookaccessor.WorkbookWriter;
+
+import net.sf.rubycollect4j.RubyFile;
 import tw.edu.ym.lab525.chart.DataTable;
 import tw.edu.ym.lab525.service.ApiService;
 
@@ -53,6 +71,38 @@ public class ApiController {
   @RequestMapping(value = "/line-chart", method = GET)
   DataTable lineChart() {
     return apiService.LineChart();
+  }
+
+  @RequestMapping(value = "/test", method = RequestMethod.GET)
+  public void queryMethod(@RequestParam String id,
+      @RequestParam Map<String, String> queryParameters,
+      @RequestParam MultiValueMap<String, String> multiMap) {
+    System.out.println("id=" + id);
+    System.out.println(queryParameters);
+    System.out.println(multiMap);
+  }
+
+  @RequestMapping(value = "/downloadExcel", method = RequestMethod.GET)
+  public void downloadExcel(HttpServletResponse response,
+      @RequestParam Map<String, String> queryParameters,
+      @RequestParam MultiValueMap<String, String> multiMap) throws IOException {
+
+    List<Map<String, String>> maps = newArrayList();
+    maps.addAll(Arrays.asList(queryParameters));
+
+    System.out.println(maps);
+
+    WorkbookWriter writer = WorkbookWriter.openXLSX().setSheetName("test");
+    for (String key : queryParameters.keySet()) {
+      writer.addRow(key, queryParameters.get(key));
+    }
+    String fileName = "TestExcelFile-" + new Date() + ".xls";
+    InputStream is = new FileInputStream(writer.save(fileName));
+    IOUtils.copy(is, response.getOutputStream());
+    response.setHeader("Content-disposition",
+        "attachment; filename=" + fileName);
+    response.flushBuffer();
+    RubyFile.delete(fileName);
   }
 
 }
